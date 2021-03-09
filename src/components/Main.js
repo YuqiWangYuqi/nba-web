e673a26553559f5d0909573f61e31dc923ca9f1e
yuqiwangyuqi <wangyuqi033@gmail.com>
2021-03-08 23:59:53 -0800

    fetch data from nba.com in main.js Instance data is damian lillard

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import Profile from "./Profile";
+import nba from '../nba-client';
+import Profile from './Profile';
 
 class Main extends Component {
+    state = {
+        playerInfo: {}
+    }
+
+    componentDidMount() {
+        window.nba = nba;
+        nba.stats.playerInfo({PlayerID: nba.findPlayer('Damian ' +
+                'Lillard').playerId}).then((info) => {
+                    console.log(info);
+                    const playInfo =
+                        Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                    console.log(playInfo);
+                    this.setState({playerInfo: playInfo});
+        })
+    }
+
     render() {
         return (
           <div className="main">
-              <Profile />
+              <Profile playerInfo={this.state.playerInfo} />
           </div>
         );
     }
