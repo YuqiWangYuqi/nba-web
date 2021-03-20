85345b8b46ce29bf38c25d4a666aa73775078a5d
yuqiwangyuqi <wangyuqi033@gmail.com>
2021-03-19 19:46:38 -0700

    update DataViewContainer to render counterSlider

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
-import ShotChart from "./ShotChart";
+import ShotChart from './ShotChart';
+import CounterSlider from './CounterSlider'
 
 class DataViewContainer extends Component {
+    state = {
+        minCount: 2
+    }
+
+    onCountSliderChange = (count) => {
+        this.setState({ minCount: count });
+    }
+
     render() {
-        return(
+        return (
             <div className="data-view">
-                <ShotChart playerId={this.props.playerId} />
+                <ShotChart playerId={this.props.playerId}
+                           minCount={this.state.minCount}
+                />
+
+                <div className="filters">
+                    <CounterSlider value={this.state.minCount}
+                                   onCountSliderChange={_.debounce(this.onCountSliderChange, 500)}/>
+                </div>
             </div>
         );
     }
